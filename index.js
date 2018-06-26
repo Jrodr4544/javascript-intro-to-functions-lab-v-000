c90ec0ea89ec1e0d144f48ecb38e366c1537dfd7
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-06-26 07:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(str) {
 function sayHiToGrandma(str) {
   if (str === whisper(str)) {
     return "I can't hear you!" + str;    
-  } else if (str === str.upperCase()) {
+  } else if (str === shout(str)) {
     return "YES INDEED!";
   } else {
     return "I love you, too."
